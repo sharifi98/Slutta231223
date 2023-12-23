e5fa1a3150978d1390b62ee820a96c09c0c2426e
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-12-23 14:18:02 +0100

    Initial Commit

---
+++ Slutta231223/Slutta231223App.swift
@@ -0,0 +1,17 @@
+//
+//  Slutta231223App.swift
+//  Slutta231223
+//
+//  Created by Hossein Sharifi on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Slutta231223App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
